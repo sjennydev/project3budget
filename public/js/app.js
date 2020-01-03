ab958f40a7277477b5030083a8d9b4ae705b7b19
Federico Salinas <federicosalinas@federicos-air.lan>
2020-01-02 18:45:33 -0600

    bill display data and expense display data

--- public/js/app.js
+++ public/js/app.js
@@ -45,6 +45,48 @@ class App extends React.Component {
                         <input type='submit'/>
                     </form>
                 </div>
+                <div className='row'>
+                  <div className='col'>
+                      <h3>Bill data</h3>
+                      <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Amount</th>
+                        <th scope="col">Paid</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                            <tr>
+                                <th scope="row">Tmobile</th>
+                                <td>$120</td>
+                                <td>Paid</td>
+                                <td><button>DELETE</button></td>
+                            </tr>
+                </tbody>
+            </table>
+                  </div>
+                  <div className='col'>
+                      <h3>Expenses data</h3>
+                      <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Amount</th>
+                        <th scope='col'>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                            <tr>
+                                <th scope="row">Tmobile</th>
+                                <td>$120</td>
+                                <td><button>DELETE</button></td>
+                            </tr>
+                </tbody>
+            </table>
+                  </div>
+                </div>
 
             </div>
           </div>
